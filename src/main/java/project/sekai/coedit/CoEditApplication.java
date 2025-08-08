ad8e760ddc4f49f747d42d9123983e534e065a49
UwU <uwu.fionia@gmail.com>
2025-08-08 22:53:47 +0800

    init:init

---
+++ src/main/java/project/sekai/coedit/CoEditApplication.java
@@ -0,0 +1,13 @@
+package project.sekai.coedit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoEditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoEditApplication.class, args);
+    }
+
+}
